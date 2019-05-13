46a213934ca0a8f90ada5b770d35f0a12cff2f6f
heyong <hy@sasucen.com>
2019-05-13 10:52:11 +0800

    Initial Commit

---
+++ PageViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PageViewDemo
+//
+//  Created by heyong on 2019/5/13.
+//  Copyright © 2019年 liuxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
